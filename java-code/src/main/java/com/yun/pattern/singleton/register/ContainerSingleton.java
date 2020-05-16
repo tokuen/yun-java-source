539530076233dc0a3c0b2660b865d39f398c16b4
guoyongyue <guoyongyue.charles@shandagames.com>
2020-05-16 09:29:05 +0800

    add
    ADD

--- java-code/src/main/java/com/yun/pattern/singleton/register/ContainerSingleton.java
+++ java-code/src/main/java/com/yun/pattern/singleton/register/ContainerSingleton.java
@@ -1,19 +1,25 @@
 package com.yun.pattern.singleton.register;
 
+import java.lang.reflect.Constructor;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ContainerSingleton {
     private static Map instanceMaps = new ConcurrentHashMap<String,Object>();
 
+    private ContainerSingleton(){
+
+    }
+
     public static Object getInstance(String clazzName){
         if(!instanceMaps.containsKey(clazzName)) {
             synchronized(ContainerSingleton.class){
                 try {
                     Class<?> clazz = Class.forName(clazzName);
-                    Object o = clazz.newInstance();
-        
-
+                    Constructor<?> constructor = clazz.getDeclaredConstructor();
+                    Object object = constructor.newInstance();
+                    instanceMaps.put(clazzName,object);
+                     return object;
                 }catch(Exception e){
                     e.printStackTrace();
                 }
